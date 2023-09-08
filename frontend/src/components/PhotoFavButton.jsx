cbac82ffaef19bb566f703f36a536d81e045ee06
Noah McMillan <noahm27@gmail.com>
2023-09-07 23:47:00 -0700

    added items and favorite button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,14 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [liked, setLiked] = useState(false);
+  const handleClick = () => { setLiked((liked === true) ? false : true); };
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <div onClick={handleClick}>
+          <FavIcon displayAlert={false} selected={liked} />
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
+const PhotoListItem = (props) => {
+  const { imageSource, profile, username } = props.photoItem;
+  const { city, country } = props.photoItem.location;
+  return (
+    <div className="photo-list__item">
+      <PhotoFavButton />
+      <img className="photo-list__image" src={imageSource} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={profile} />
+        <div className="photo-list__user-info">
+          {username}
+          <div className="photo-list__user-location">{city}, {country}</div>
+        </div>
+      </div>
+    </div>
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  );
 };
 
 export default PhotoListItem;
